3d95572e20e601d742e8e05d18c4b0f3653e4ab3
xsaulsouza-cmd <xsaulsouza@gmail.com>
2026-03-05 16:02:48 -0400

    Update service worker cache and file list

--- sw.js
+++ sw.js
@@ -1,20 +1,36 @@
-const CACHE = 'casa-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'casa-v2';
+const FILES = [
+  '/casa-checklist/',
+  '/casa-checklist/index.html',
+  '/casa-checklist/manifest.json',
+  '/casa-checklist/icons/icon-192.png',
+  '/casa-checklist/icons/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {}))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('/casa-checklist/index.html')))
   );
 });
